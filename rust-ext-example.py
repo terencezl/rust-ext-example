7bb537bbeeddeb8cf6457b308e4860de5e037f92
Terence Z. Liu <terence.z.liu@gmail.com>
2024-10-08 12:12:04 -0500

    take_filepath() that reads and deserializes in Rust

--- rust-ext-example.py
+++ rust-ext-example.py
@@ -31,7 +31,7 @@ def take_iter_py(iterator: Iterator[bytes], np_vectors: np.ndarray) -> int:
             )
             np_vectors[idx] = vector
         except ValueError:
-            print(f"array size does not match at {idx}!")
+            print("Array size does not match!")
             continue
         idx += 1
 
@@ -48,7 +48,7 @@ def process_py(filepath):
 
     # print(f"{np_vectors[-1, :5]}")
     # logger.debug(f"take_iter_py took {time.time() - t:.2f} seconds.")
-    return np_vectors
+    return np_vectors  # comment out if memory consumption too big
 
 
 def process_rs(filepath):
@@ -61,14 +61,27 @@ def process_rs(filepath):
 
     # print(f"{np_vectors[-1, :5]}")
     # logger.debug(f"rust_ext.take_iter took {time.time() - t:.2f} seconds.")
-    return np_vectors
+    return np_vectors  # comment out if memory consumption too big
+
+
+def process_rs_full(filepath):
+    t = time.time()
+    np_vectors = np.empty(
+        (COUNT_PER_MSGPACK_UPPERBOUND, SIZE_ARRAY_DIM), dtype=np.float32
+    )
+    count = rust_ext.take_filepath(filepath, np_vectors)
+    np_vectors = np_vectors[:count]
+
+    # print(f"{np_vectors[-1, :5]}")
+    # logger.debug(f"rust_ext.take_iter took {time.time() - t:.2f} seconds.")
+    return np_vectors  # comment out if memory consumption too big
 
 
 if __name__ == "__main__":
     filepath = "bytes_vectors.msgpack"
 
     N_tasks = 32
-    N_workers = 8
+    N_workers = 4
 
     pool = ProcessPoolExecutor(max_workers=N_workers)
 
@@ -102,6 +115,14 @@ if __name__ == "__main__":
     futures = []
     for _ in range(N_tasks):
         futures.append(pool.submit(process_rs, filepath))
+
+    for future in tqdm(futures):
+        _ = future.result()
+
+    logger.info("With Rust extension - full / thread pool:")
+    futures = []
+    for _ in range(N_tasks):
+        futures.append(pool.submit(process_rs_full, filepath))
 
     for future in tqdm(futures):
         _ = future.result()
--- take_iter/src/lib.rs
+++ take_iter/src/lib.rs
@@ -1,13 +1,16 @@
+use anyhow::Result;
 use numpy::ndarray::{s, ArrayViewMut1};
 use numpy::PyReadwriteArray2;
-use pyo3::exceptions::PyValueError;
+use pyo3::exceptions::{PyRuntimeError, PyValueError};
 use pyo3::prelude::*;
 use pyo3::types::{PyBytes, PyIterator};
+use std::fs::File;
+use std::io::{BufReader, Read};
 
 const SIZE_ARRAY_DIM: usize = 512;
 const F32_SIZE: usize = 4;
 
-fn copy_array(src_bytes_vector: &[u8], dst_vector: &mut ArrayViewMut1<f32>) -> Result<(), String> {
+fn copy_array(src_bytes_vector: &[u8], dst_vector: &mut ArrayViewMut1<f32>) -> Result<()> {
     if src_bytes_vector.len() == dst_vector.len() * F32_SIZE {
         // f32 from msgpack
 
@@ -31,12 +34,46 @@ fn copy_array(src_bytes_vector: &[u8], dst_vector: &mut ArrayViewMut1<f32>) -> R
         }
         return Ok(());
     } else {
-        return Err(format!(
+        anyhow::bail!(
             "Array size is {}, does not match {}!",
             src_bytes_vector.len(),
             dst_vector.len() * F32_SIZE
-        ));
+        );
     };
+}
+
+struct RawListIterator {
+    reader: BufReader<File>,
+}
+
+impl RawListIterator {
+    fn new(filepath: &str) -> Result<Self> {
+        let file = File::open(filepath)?;
+        let reader = BufReader::new(file);
+        Ok(RawListIterator { reader })
+    }
+}
+
+impl Iterator for RawListIterator {
+    type Item = Result<Vec<u8>>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match rmp::decode::read_bin_len(&mut self.reader) {
+            Ok(len) => {
+                let mut buffer = vec![0u8; len as usize];
+                match self.reader.read_exact(&mut buffer) {
+                    Ok(_) => Some(Ok(buffer)),
+                    Err(e) => Some(Err(e.into())),
+                }
+            }
+            Err(rmp::decode::ValueReadError::InvalidMarkerRead(e))
+                if e.kind() == std::io::ErrorKind::UnexpectedEof =>
+            {
+                None
+            }
+            Err(e) => Some(Err(e.into())),
+        }
+    }
 }
 
 #[pymodule]
@@ -72,6 +109,45 @@ fn rust_ext(m: &Bound<PyModule>) -> PyResult<()> {
 
             let mut idx = 0;
             for src_bytes_vector in raw_list {
+                let mut dst_vector = vectors.slice_mut(s![idx, ..]);
+                if let Err(e) = copy_array(&src_bytes_vector, &mut dst_vector) {
+                    eprintln!("Error: {}. Skipping...", e);
+                    continue;
+                }
+                idx += 1;
+            }
+            Ok(idx)
+        })
+    }
+
+    #[pyfn(m)]
+    fn take_filepath(
+        py: Python,
+        filepath: &str,
+        mut np_vectors: PyReadwriteArray2<f32>,
+    ) -> PyResult<usize> {
+        let mut vectors = np_vectors.as_array_mut();
+
+        // Bytes are read as f32 arrays and copied into the passed in NumPy array.
+        // We release the GIL here so other Python threads can run in true parallelism.
+        py.allow_threads(|| {
+            let raw_list_iter = RawListIterator::new(filepath)
+                .map_err(|e| PyRuntimeError::new_err(e.to_string()))?;
+            if vectors.dim().1 != SIZE_ARRAY_DIM {
+                return Err(PyValueError::new_err(format!(
+                    "2D NumPy array has {} columns, does not match {}!",
+                    vectors.dim().1,
+                    SIZE_ARRAY_DIM
+                )));
+            }
+
+            let mut idx = 0;
+            for src_bytes_vector in raw_list_iter {
+                let src_bytes_vector =
+                    src_bytes_vector.map_err(|e| PyValueError::new_err(e.to_string()))?;
+                if idx >= vectors.dim().0 {
+                    return Err(PyValueError::new_err("Too many items in iterator!"));
+                }
                 let mut dst_vector = vectors.slice_mut(s![idx, ..]);
                 if let Err(e) = copy_array(&src_bytes_vector, &mut dst_vector) {
                     eprintln!("Error: {}. Skipping...", e);
